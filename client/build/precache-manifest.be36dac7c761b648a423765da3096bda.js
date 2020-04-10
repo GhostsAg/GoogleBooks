5fb27d6e8f5fe06edc396933fad08a9012c42cc4
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-04-09 22:30:47 -0500

    built deployed version.

---
+++ client/build/precache-manifest.be36dac7c761b648a423765da3096bda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff2b17208a511030e6cd6d4221d644f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74a1c8f48fe988df4f0b",
+    "url": "/static/css/main.1e3be0e2.chunk.css"
+  },
+  {
+    "revision": "a3b1fba4aab9b41bd732",
+    "url": "/static/js/2.cd13caf8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd13caf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a1c8f48fe988df4f0b",
+    "url": "/static/js/main.b2f05d3e.chunk.js"
+  },
+  {
+    "revision": "b9583a7bd45ab34a60be",
+    "url": "/static/js/runtime-main.ef512771.js"
+  }
+]);
